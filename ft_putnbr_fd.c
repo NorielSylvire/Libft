ba471f8c63d82582b723e677a84f657f84653d8e
NorielSylvire <fl4v1u5@yandex.com>
2023-11-26 10:10:45 +0100

    Corrected memory leak at putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: fhongu <fhongu@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/01 19:01:26 by fhongu            #+#    #+#             */
-/*   Updated: 2023/10/04 20:21:47 by fhongu           ###   ########.fr       */
+/*   Updated: 2023/11/25 20:31:31 by fhongu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,13 @@
 
 int	ft_putnbr_fd(int n, int fd)
 {
-	char	*ret;
+	char	*nbr;
+	int		ret;
 
-	ret = ft_itoa(n);
-	if (!ret)
+	nbr = ft_itoa(n);
+	if (!nbr)
 		return (0);
-	return (ft_putstr_fd(ret, fd));
+	ret = ft_putstr_fd(nbr, fd);
+	ft_free((void **)&nbr);
+	return (ret);
 }
